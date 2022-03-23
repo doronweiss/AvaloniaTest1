b03d35b6cca10293f66e58f792921c3c358fe691
doron weiss <doron@10fs.co.il>
2022-03-23 11:00:03 +0200

    UI update

--- AvaloniaTest1/MainWindow.axaml.cs
+++ AvaloniaTest1/MainWindow.axaml.cs
@@ -9,20 +9,23 @@ namespace AvaloniaTest1 {
     private DispatcherTimer timer = null;
     public MainWindow() {
       InitializeComponent();
+      timer = new DispatcherTimer();
+      timer.Tick += new EventHandler(this.OnTimer);
+      timer.Interval = TimeSpan.FromMilliseconds(250);
     }
 
     public void OnBtnClick(object? sender, RoutedEventArgs routedEventArgs) {
       //myLBL.Content = DateTime.Now.ToString();
-      if (timer == null) {
-        timer = new DispatcherTimer();
-        timer.Tick += new EventHandler(this.OnTimer);
-        timer.Interval = TimeSpan.FromMilliseconds(250);
-        timer.Start();
+      if (!timer.IsEnabled) {
+        timer.IsEnabled = true;
+      } else {
+        timer.IsEnabled = false;
+        myLBL.Text = $"Stopped: {DateTime.Now.ToString()}";
       }
     }
 
     private void OnTimer(object? sender, EventArgs e) {
-      myLBL.Content = DateTime.Now.ToString();
+      myLBL.Text = DateTime.Now.ToString();
     }
 
     private void OnWindowClosing(object? sender, CancelEventArgs e) {
